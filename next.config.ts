b776ee63a249f214e483b2cd6c6e888fbdd5a763
Syahrul Apriansyah <apriansyah.syahrul@gmail.com>
2025-12-19 20:28:15 +0700

    add internal reverse proxy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/socket.io/:path*",
+        destination: "http://websocket:3001/socket.io/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
